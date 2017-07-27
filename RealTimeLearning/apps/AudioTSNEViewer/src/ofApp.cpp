370b0437a3fedfdeb0a916d4d661f1b20abcef66
DUCK HA HWANG <personableduck@gmail.com>
2017-07-27 00:22:38 -0700

    feat: Initial commit

---
+++ RealTimeLearning/apps/AudioTSNEViewer/src/ofApp.cpp
@@ -0,0 +1,140 @@
+#include "ofApp.h"
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    // this should point to the json file containing your audio files and tsne coordinates
+    // follow instructions in ml4a.github.io/guides/AudioTSNEViewer/
+    tsnePath = ofToDataPath("audiotsne.json");
+    
+    bLoad.addListener(this, &ofApp::eLoad);
+    gui.setup();
+    gui.setName("Audio t-SNE");
+    gui.add(maxDuration.set("maxDuration", 1.0, 0.1, 2.0));
+    gui.add(mouseRadius.set("mouseRadius", 250, 100, 500));
+    gui.add(pauseLength.set("pauseLength", 2.0, 0.2, 5.0));
+    gui.add(bLoad.setup("Load model"));
+
+    load(tsnePath);
+}
+
+//--------------------------------------------------------------
+void ofApp::eLoad() {
+    ofFileDialogResult result = ofSystemLoadDialog("Which xml file to load?", true);
+    if (result.bSuccess) {
+        load(result.filePath);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::load(string filename) {
+    tsnePath = filename;
+    ofxJSONElement result;
+    parsingSuccessful = result.open(tsnePath);
+    sounds.clear();
+    for (int i=0; i<result.size(); i++) {
+        string path = result[i]["path"].asString();
+        float x = result[i]["point"][0].asFloat();
+        float y = result[i]["point"][1].asFloat();
+        AudioClip newSound;
+        newSound.sound.load(path);
+        newSound.point.set(x, y);
+        newSound.t = 0;
+        sounds.push_back(newSound);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    if (!parsingSuccessful) {
+        return;
+    }
+    
+    for (int i=0; i<sounds.size(); i++) {
+        if (sounds[i].sound.isPlaying() && sounds[i].sound.getPositionMS() > maxDuration*1000) {
+            sounds[i].sound.stop();
+        }
+    }
+    ofSoundUpdate();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackgroundGradient(ofColor(100), ofColor(20));
+    if (!parsingSuccessful) {
+        ofDrawBitmapString("Could not find file "+tsnePath+"\nSee the instructions for how to create one.", 50, 50);
+        return;
+    }
+    for (int i=0; i<sounds.size(); i++) {
+        if (sounds[i].sound.isPlaying()) {
+            ofSetColor(0, 255, 0, 180);
+        }
+        else {
+            ofSetColor(255, 180);
+        }
+        ofDrawCircle(ofGetWidth() * sounds[i].point.x, ofGetHeight() * sounds[i].point.y, 4);
+
+    }
+    gui.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    for (int i=0; i<sounds.size(); i++) {
+        float distanceToMouse = ofDistSquared(x, y, ofGetWidth() * sounds[i].point.x, ofGetHeight() * sounds[i].point.y);
+        if (distanceToMouse < mouseRadius && !sounds[i].sound.isPlaying() && (ofGetElapsedTimef() - sounds[i].t > pauseLength)) {
+            sounds[i].t = ofGetElapsedTimef();
+            sounds[i].sound.play();
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
---
+++ RealTimeLearning/apps/ConvnetPredictor/src/ofApp.h
@@ -0,0 +1,81 @@
+#pragma once
+
+// where to send osc messages by default
+#define DEFAULT_OSC_DESTINATION "localhost"
+#define DEFAULT_OSC_ADDRESS "/wek/outputs"
+#define DEFAULT_OSC_PORT 12000
+#define DEFAULT_DEVICE_ID 0
+#define SIZE_INPUT_VECTOR 4096
+
+//#define RELEASE
+
+#include "ofMain.h"
+#include "ofxGrt.h"
+#include "ofxGui.h"
+#include "ofxOsc.h"
+#include "ofxCcvThreaded.h"
+#include "PipelineThreaded.h"
+
+
+class ofApp : public ofBaseApp {
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    
+    void updateParameters();
+    RegressionThreaded * addSlider();
+    CategoricalThreaded * addCategorical(int numClasses);
+    void train();
+    
+    void setupOSC();
+    void sendOSC();
+    
+    void save(string modelName);
+    void load(string modelPath);
+    void clear();
+    
+    void eSlider(float & v);
+    void eAddSlider();
+    void eAddCategorical();
+    void eCategorical(int & v);
+    void eChangeOscSettings();
+    void eChangeCamera();
+    void eSave();
+    void eLoad();
+    
+    void exit();
+    
+    // learning
+    vector<PipelineThreaded*> learners;
+    vector<ofParameter<float> > sliders, targetSliders;
+    vector<int> categoricals;
+
+    // input/cv
+    ofVideoGrabber cam;
+    ofxCcvThreaded ccv;
+    vector<float> featureEncoding;
+    bool isTraining;
+    int numSamples;
+    
+    // draing/ui
+    string infoText;
+    ofTrueTypeFont largeFont;
+    
+    // parameters
+    ofParameter<float> lerpAmt;
+    ofParameter<int> maxEpochs, numHiddenNeurons;
+    ofParameter<string> gOscDestination, gOscAddress, gOscPort, gDeviceId;
+    
+    // gui
+    ofxPanel gui, guiSliders;
+    ofParameterGroup gOscSettings, gTraining;
+    ofxButton bTrain, bSave, bLoad, bClear, bAddSlider, bAddCategorical, bOscSettings, bCameraSettings;
+    ofxToggle tRecord, tPredict;
+    
+    // osc
+    ofxOscSender sender;
+    string oscDestination, oscAddress;
+    int oscPort;
+};
